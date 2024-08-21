c191b1363de77f7fac4ed45543de85d005703c4a
digberi <orkepuma@gmail.com>
2024-08-21 12:46:07 +0300

    v1.0.2

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as SharesWallet from './wrappers/SharesWallet';
 import * as TonJettonTonStrategy from './wrappers/TonJettonTonStrategy';
 import * as Vault from './wrappers/Vault';
 import * as VaultFactory from './wrappers/VaultFactory';
-
+//
 export {
   SharesWallet,
   TonJettonTonStrategy,
